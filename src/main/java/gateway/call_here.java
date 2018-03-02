a3a1b15f90c4fe34f24a113a67e3daef3496ad70
Your Name <you@example.com>
2018-03-02 10:33:10 +0800

    ci

--- src/main/java/gateway/call_here.java
+++ src/main/java/gateway/call_here.java
@@ -2,7 +2,7 @@ package gateway;
 
 public class call_here
 {
-	public static void loop() throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 		while(true){
 			System.out.println("Hello");
 			Thread.sleep(10000);
